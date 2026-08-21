3125c704daea82ead3e3821633da306a4eae9663
bgl gwyng <bgl@gwyng.com>
2026-08-22 01:32:12 +0900

    Fix packages-nix generator sandbox environment

--- nix/generate-packages-nix.nix
+++ nix/generate-packages-nix.nix
@@ -2,6 +2,9 @@
 
 { package-set }:
 pkgs.runCommand "packages-nix" { nativeBuildInputs = [ nixXCabalUtils pkgs.nix ]; } ''
+  export HOME="$TMPDIR"
+  export LC_ALL=C.UTF-8
+
   generate-packages-nix \
     --nixpkgs ${pkgs.path} \
     --package-set ${package-set} \
